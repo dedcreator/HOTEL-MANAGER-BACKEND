67faf7517d62a0ed534669cb55c2cd4187be147a
oduagbondestiny <oduagondestiny@gmail.com>
2026-04-05 23:11:18 +0100

    fixing inventory errors

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -206,6 +206,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://172.20.10.4:3000",
+    "loca",
 ]
 
 # ====== EMAIL CONFIGURATION (BREVO) ======
